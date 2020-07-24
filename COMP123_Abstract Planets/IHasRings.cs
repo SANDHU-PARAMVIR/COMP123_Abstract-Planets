6abba0324afe3e69e091987e8d41be9d238950ae
Param Vir Singh Sandhu <psandh63@my.centennialcollege.ca>
2020-07-24 00:27:52 -0400

    Fixed minor issues

--- COMP123_Abstract Planets/IHasRings.cs
+++ COMP123_Abstract Planets/IHasRings.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace COMP123_Abstract_Planets
 {
-    //This interface is used by the Planet classes and returns a boolean variable depending on whether or not
+    //This interface is used by the Planet class and returns a boolean variable depending on whether or not
     //a planet has Rings.
     public interface IHasRings
     {
